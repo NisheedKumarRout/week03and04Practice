93bd9f777067c2fbb65aeefca23dd209ec74ff68
NisheedKumarRout <nisheedkumarrout@gmail.com>
2026-03-24 15:47:08 +0530

    Problem4 Portfolio sorting implemented

---
+++ untitled/src/Problem4.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Problem4 {
+    public static void main(String[] args) {
+        double[] arr = {12, 8, 15};
+        Arrays.sort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+}
